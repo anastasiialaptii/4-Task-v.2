11870fdfc0dbf98ab96d7c2d0d188f89e7bf4d33
Anastasia Laptii <laptiyanastasia@gmail.com>
2020-04-04 02:28:04 +0300

    delete useless chunks of code also add testProduct to get list details

--- Task4v2/Controllers/ProductController.cs
+++ Task4v2/Controllers/ProductController.cs
@@ -6,19 +6,7 @@ namespace Task4v2.Controllers
 {
     public class ProductController : Controller
     {
-        private ProductManager productManager = new ProductManager();
-        private ProductDataManager productDataManager = new ProductDataManager();
         private ProductSessionManager sessionManager = new ProductSessionManager();
-
-        public ActionResult ProductListView()
-        {
-            return View(productDataManager.productList);
-        }
-
-        public ActionResult ProductListDetails(string name)
-        {
-            return View(productManager.ProductDetails(productDataManager.productList, name));
-        }
 
         public ActionResult CreateProduct()
         {
--- Task4v2/Managers/ProductDataManager.cs
+++ Task4v2/Managers/ProductDataManager.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using Task4v2.Models;
 
 namespace Task4v2.Managers
